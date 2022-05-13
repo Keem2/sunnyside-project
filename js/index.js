dfddbaf10b5ba3defe893618e579e05c6573b8ff
Akeem <asmith.339@outlook.com>
2022-05-13 05:49:12 -0400

    Updated Javascript

--- js/index.js
+++ js/index.js
@@ -5,12 +5,5 @@ let menu = document.getElementById("mobile");
 hamburger.addEventListener('click', showMobileMenu);
 
 function showMobileMenu() {
-    // menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
-    if (menu.style.display === 'none') {
-        menu.style.display = 'block';
-        menu.style.animation = 'ani 1s';
-    } else {
-        menu.style.display = 'none';
-        menu.style.animation = 'ani-back 1s';
-    }
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
 }
