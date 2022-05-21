7497326be32413c0f07d056e669f80f1777b75e4
Akeem <asmith.339@outlook.com>
2022-05-21 10:28:27 -0400

    Added animation to mobile menu

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 //hamburger 
 let hamburger = document.getElementById("ham-a");
 let menu = document.getElementById("mobile");
-
-hamburger.addEventListener('click', showMobileMenu);
+menu.style.display = 'none';
 
 function showMobileMenu() {
-    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block';
+    }
+
+    if (menu.classList.contains('ani') == true) {
+        menu.classList.remove('ani');
+        menu.classList.add('ani-back');
+    }
+    else {
+        menu.classList.remove('ani-back');
+        menu.classList.add('ani');
+    }
 }
+
+hamburger.addEventListener('click', showMobileMenu);
